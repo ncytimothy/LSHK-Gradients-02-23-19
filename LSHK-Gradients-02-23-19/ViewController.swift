ba26e727ffa279b704f049db36ee7d2f43adbc59
Tim Ng <timothyng2008@me.com>
2019-02-16 14:20:12 +0800

    feat: First step to add graidentLayer to the ViewController's view

--- LSHK-Gradients-02-23-19/ViewController.swift
+++ LSHK-Gradients-02-23-19/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        createGradientLayer()
+    }
+    
+    fileprivate func createGradientLayer() {
+        
+        // 1. Initialize the gradient layer
+        let gradientLayer = CAGradientLayer()
+        
+        // 2. Set the layer's frame
+        gradientLayer.frame = self.view.frame
+        
+        // 3. Set colors to produce the gradient layer
+        gradientLayer.colors = [UIColor.red.cgColor, UIColor.yellow.cgColor]
+        
+        // 4. Add gradient layer as sublayer of your target view's layer
+        self.view.layer.addSublayer(gradientLayer)
+        
+    }
 
 }
 

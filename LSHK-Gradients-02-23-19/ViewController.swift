37a39e0acd3dd1584704e2aa1f1fe7e93f68a42c
Tim Ng <timothyng2008@me.com>
2019-02-16 14:09:19 +0800

    Initial commit

---
+++ LSHK-Gradients-02-23-19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LSHK-Gradients-02-23-19
+//
+//  Created by Tim Ng on 2/16/19.
+//  Copyright © 2019 timothyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
